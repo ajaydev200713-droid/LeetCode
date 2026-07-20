b8e65eb74bbca55a82be68d8a47dc688721f89c1
Ajaydev <ajaydev2007.13@gmail.com>
2026-07-20 10:14:45 +0530

    LC-sqrtx [Runtime: N/A; Memory: N/A]

--- math/sqrtx.cpp
+++ math/sqrtx.cpp
@@ -2,24 +2,28 @@ class Solution {
 public:
     int mySqrt(int x) 
     {
-        if(x<=0)
+        if(x == 0)
             return 0;
         int low = 1;
         int high = x;
         int ans = 1;
-        while(low<high)
+        while(low <= high)
         {
             int mid = low + (high - low)/2;
-            if(mid*mid <= x)
+            long long sq = mid*mid;
+            if(sq == x)
             {
-                ans = mid;
-                low = mid+1;
+                return mid;
+            }
+            else if(sq < x)
+            {
+                low = mid + 1;
             }
             else
             {
-                high = mid -1;
+                high = mid - 1;
             }
-        }    
-        return ans;
+        }  
+        return high;  
     }
 };
