65fe55f8b77d390ca27d9cd28615429ca570efbd
Ajaydev <ajaydev2007.13@gmail.com>
2026-08-13 18:32:05 +0530

    LC-trapping-rain-water [Runtime: N/A; Memory: N/A]

---
+++ stack/trapping-rain-water.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int trap(vector<int>& height) 
+    {
+        int n = height.size();
+        int leftmax[n];
+        int rightmax[n];
+        leftmax[0] = height[0];
+        rightmax[n-1] = height[n-1];
+        for(int i=1;i<n;i++)
+            leftmax[i] = max(height[i] , leftmax[i-1]);
+        for(int i=n-2;i>=0;i--)
+            rightmax[i] = max(height[i] , rightmax[i+1]);
+        int total = 0;
+        for(int i=0;i<n;i++)
+            total += min(leftmax[i] , rightmax[i]) - height[i]; 
+        return total;
+    }
+};
