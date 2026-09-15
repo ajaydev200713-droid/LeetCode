bb2ba35f56c8683226994a011595c2bd830af964
Ajaydev <ajaydev2007.13@gmail.com>
2026-09-15 21:53:35 +0530

    LC-first-unique-character-in-a-string [Runtime: N/A; Memory: N/A]

---
+++ string/first-unique-character-in-a-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n = s.size();
+        unordered_map<char,int>mpp;
+        for(char ch : s)
+        {
+            mpp[ch]++;
+        }    
+        for(int i=0;i<n;i++)
+        {
+            if(mpp[s[i]] == 1)
+                return i;
+        }
+        return -1;
+    }
+};
